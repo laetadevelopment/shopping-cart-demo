2532ef765efaaaa359ca16850c550496a7256be7
Mike Laeta <mike@laetadevelopment.com>
2020-03-10 23:09:13 -0600

    debug state logic

--- pwa/src/components/mixin/addItem.js
+++ pwa/src/components/mixin/addItem.js
@@ -6,27 +6,29 @@ export default (url, title) => {
   let d = new Date();
   let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-  firebase.db.collection('items').add( {
-    url,
-    title,
-    created_at: new Date().getTime(),
-    updated_at: new Date().getTime()
-  })
-
-  console.log(store.state.id)
-
   if (store.state.id === '') {
-    firebase.db.collection('items').orderBy('created_at').limitToLast(1).onSnapshot((snapShot) => { 
-      snapShot.forEach((item)  => {
-        firebase.db.collection('carts').add(
-          {
-            items: "id:" + item.id + ";qty:1",
-            created_at: new Date().getTime(),
-            updated_at: new Date().getTime()
-          }
-        )
-        router.push({ name: 'cart'})
-      });
-    });
+    store.commit('setId', '12345')
+    firebase.db.collection('items').add( {
+      url,
+      title,
+      created_at: new Date().getTime(),
+      updated_at: new Date().getTime()
+    })
+    firebase.db.collection('carts').add(
+      {
+        items: "id:12345;qty:1",
+        created_at: new Date().getTime(),
+        updated_at: new Date().getTime()
+      }
+    )
+    router.push({ name: 'cart'})
+  } else {
+    firebase.db.collection('items').add( {
+      url,
+      title,
+      created_at: new Date().getTime(),
+      updated_at: new Date().getTime()
+    })
+    router.push({ name: 'cart'})
   }
 }
